8ff2b0e7c61ef55c6503551202da9ad2e5f9e8a1
hackingnote <hackingnote.contact@gmail.com>
2018-12-27 17:36:30 -0800

    update 2018-12-27

--- build/LintCode_files/base.js
+++
@@ -1,2 +0,0 @@
-$('.language').on('click',function(e){var lang=$(this).attr('value')
-$('#i18n_language').val(lang);return $('#i18n_form').submit();});$(document).ready(function(){$('.utctime').each(function(){const utctime=$(this).html().trim();const date=new Date(utctime);$(this).html(date.toLocaleDateString()+' '+ date.toLocaleTimeString());})});
--- build/LintCode_files/demo.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function()
-{$('#nestable1').nestable({group:1});$('#nestable2').nestable({group:1});var $expand=false;$('#nestable-menu').on('click',function(e)
-{if($expand){$expand=false;$('.dd').nestable('expandAll');}else{$expand=true;$('.dd').nestable('collapseAll');}});$('#nestable3').nestable();});

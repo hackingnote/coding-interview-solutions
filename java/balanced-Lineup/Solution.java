98240d48df9ba91b1b0aab0702517a88bad7f4e5
hackingnote <hackingnote.contact@gmail.com>
2022-03-30 16:03:39 -0700

    update 2022-03-30

--- java/balanced-Lineup/Solution.java
+++ java/balanced-Lineup/Solution.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class POJ3264 {
+public class Solution {
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
     int N = Integer.parseInt(in.next());

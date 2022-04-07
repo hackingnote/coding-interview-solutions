b603d7094b96b9950c677394288b47b679b620dd
hackingnote <hackingnote.contact@gmail.com>
2022-04-07 09:59:51 -0700

    update 2022-04-07

--- cpp/rotate_array.cc
+++ cpp/rotate_array.cc
@@ -7,38 +7,6 @@ class Solution {
     k %= nums.size();
     if (k == 0) return;
     int i, j, tmp;
-    for (i = 0; i < k; i++) {
-      tmp = nums[nums.size() - 1];
-      for (j = nums.size() - 1; j > 0; j--) {
-        nums[j] = nums[j - 1];
-      }
-      nums[0] = tmp;
-    }
-  }
-};
-
-class Solution {
- public:
-  void rotate(std::vector<int> &nums, int k) {
-    if (nums.empty()) return;
-    k %= nums.size();
-    if (k == 0) return;
-    int i;
-    std::vector<int> tmp;
-    for (i = 0; i < nums.size(); i++) tmp.push_back(nums[i]);
-    for (i = 0; i < tmp.size(); i++) {
-      nums[(i + k) % nums.size()] = tmp[i];
-    }
-  }
-};
-
-class Solution {
- public:
-  void rotate(std::vector<int> &nums, int k) {
-    if (nums.empty()) return;
-    k %= nums.size();
-    if (k == 0) return;
-    int i, j, tmp;
     for (i = 0, j = nums.size() - 1; i < j; i++, j--) {
       tmp = nums[i];
       nums[i] = nums[j];

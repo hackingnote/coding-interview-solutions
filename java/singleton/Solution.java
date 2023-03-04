44cf9e0b551909016efd06890f5f1c10cc3f60e9
hackingnote <hackingnote.contact@gmail.com>
2023-03-04 13:01:31 -0800

    update 2023-03-04

--- java/singleton/Solution.java
+++
@@ -1,8 +0,0 @@
-class Solution {
-  /** @return: The same instance of this class every time */
-  private static Solution solution = new Solution();
-
-  private Solution() {}
-
-  public static Solution getInstance() { return solution; }
-};
